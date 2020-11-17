934d6a8f4621894e88ddb9efb427fa5ec46c69db
Sebastian Alexis Restrepo Hoyos <69174291+SebastianR2904@users.noreply.github.com>
2020-11-17 10:26:26 -0500

    Images Imágenes agregadas, ya que no se mostraba ninguna imagen

--- src/pages/data.js
+++ src/pages/data.js
@@ -15,7 +15,7 @@ const infogimnasio = [
       },
       {
         descripcion: "5 repeticiones de levantamineto de Pesas - 4 series",
-        imagen: require("../images/brazomancuernad.jpg")
+        imagen: require("../images/barzomancuerna.jpg")
       }
     ]
   },
@@ -30,22 +30,22 @@ const infogimnasio = [
       },
       {
         descripcion: "14 repeticiones de flexion de pecho - 4 series",
-        imagen: require("../images/brazopechpd.jpg")
+        imagen: require("../images/brazopecho.jpg")
       },
       {
         descripcion: "14 repeticiones de levantamineto de Pesas - 4 series",
-        imagen: require("../images/brazomancuernad.jpg")
+        imagen: require("../images/brazomancuerna.jpg")
       }
     ]
   },
   {
     nomejercicio: "Abdomen fácil",
-    imagen: require("../images/abdofacil.jpg"),
+    imagen: require("../images/abdomen.jpg"),
     ruta: "Abdomen-Facil",
     ejercicios: [
       {
         descripcion: "10 abdominales",
-        imagen: require("../images/abdomenf.jpg")
+        imagen: require("../images/abdofacil.jpg")
       },
       {
         descripcion: "20 seg de piernas levantadas",
@@ -92,7 +92,7 @@ const infogimnasio = [
       },
       {
         descripcion: "30 abdominales de rodilla",
-        imagen: require("../images/rodillasd.gif")
+        imagen: require("../images/rodillad.jpg")
       },
       {
         descripcion: "20 toques de talon",
@@ -146,7 +146,7 @@ const infogimnasio = [
       },
       {
         descripcion: "20 sentadillas búlgara por cada pierna - 4 series",
-        imagen: require("../images/bulgarad.jpeg")
+        imagen: require("../images/bulgarad.jpg")
       },
       {
         descripcion: "30 repeticiones de puente de cadera - 4 series",
