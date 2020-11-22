c82a53d422895bd8dd8ba7488c5537d6c0c19be6
Juan Esteban Múnera David <66125352+Muneraaa@users.noreply.github.com>
2020-11-21 19:04:00 -0500

    Organizacion se organizaron unos errores que presentaba el proyecto

--- src/pages/Logins.js
+++ src/pages/Logins.js
@@ -1,7 +1,58 @@
 import React from "react";
 import "../style/style.css";
+import axios from "axios";
+import Cookies from "universal-cookie";
 
+const baseurl = "http://localhost:3001/resgistro";
+const cookies = new Cookies();
+
+let iniciarSesion = async () =>
+  await axios
+    .get(baseurl, {
+      params: {
+        usuario: this.state.form.usuario,
+        contraseña: this.state.form.contraseña
+      }
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .then((response) => {
+      if (response.length > 0) {
+        var respuesta = response[0];
+        cookies.set("id", respuesta.id, { path: "/" });
+        cookies.set("nombre_completo", respuesta.nombre_completo, {
+          path: "/"
+        });
+        cookies.set("usuario", respuesta.usuario, { path: "/" });
+        cookies.set("contraseña", respuesta.contraseña, { path: "/" });
+        cookies.set("email", respuesta.email, { path: "/" });
+
+        alert(`Bienvenido ${respuesta.usuario}`);
+        window.location.href = "./principal";
+      } else {
+        alert("El usuario o la contraseña no son correctos");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 class Logins extends React.Component {
+  state = {
+    form: {
+      usuario: "",
+      contraseña: ""
+    }
+  };
+  handleChange = async (e) => {
+    await this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
   render() {
     return (
       <div className="container-fluid fondoInicio">
@@ -10,33 +61,38 @@ class Logins extends React.Component {
             <h1 className="tituloPrincipal"> INICIO DE SESIÓN</h1>
 
             <form>
-              <label for="exampleInputEmail1">Usuario</label>
+              <label htmlFor="exampleInputEmail1">Usuario</label>
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
+                name="usuario"
+                onChange={this.handleChange}
               ></input>
             </form>
 
             <form>
-              <label for="exampleInputPassword1">Contraseña</label>
+              <label htmlFor="exampleInputPassword1">Contraseña</label>
               <input
                 type="password"
-                class="form-control"
+                className="form-control"
                 id="exampleInputPassword1"
+                name="contraseña"
+                onChange={this.handleChange}
               ></input>
             </form>
             <button type="button" className="btn btn-outline-dark btnolvcontra">
               <a href="olvidocontra">¿Olvidaste tu contraseña?</a>
             </button>
 
-            <div class="container">
-              <div class="row btnsesion">
+            <div className="container">
+              <div className="row btnsesion">
                 <div>
                   <button
                     type="button"
                     className="btn btn-outline-dark btn-lg btn-"
+                    onClick={() => this.iniciarSesion()}
                   >
                     <a href="seleccionar">Iniciar</a>
                   </button>
@@ -49,7 +105,7 @@ class Logins extends React.Component {
                     <a href="/">Atrás</a>
                   </button>
                 </div>
-                <div class="w-100"></div>
+                <div className="w-100"></div>
               </div>
             </div>
           </div>
--- src/pages/principal.js
+++ src/pages/principal.js
@@ -11,42 +11,37 @@ import data from "./data";
 class principal extends React.Component {
   render() {
     return (
-      
-        <section className="contenedorEjercicios  ">
-          <h1>ENTRENATE JMS</h1>
-          <div className="contenedorEjercicios__lista">
-            {data.infogimnasio.map((elemento, indice) => {
-              return (
-                <div className="carta">
-                  <li
-                    key={indice}
-                    className="contenedorEjercicios__lista--elemento"
-                  >
-                    {elemento.nomejercicio}
-                  </li>
-                  <img src={elemento.imagen} alt="Img"></img>
-                  {elemento.ejercicios.map((elemento, indice) => {
-                    return (
-                      <div>
-                        <li key={indice}>{elemento.descripcion}</li>
-                      </div>
-                    );
-                  })}
-                  <button
-                    type="button"
-                    className="btn btn-dark btn-lg btn-block"
-                  >
-                    <a href={elemento.ruta}>¿Cómo se hace?</a>{" "}
-                  </button>
-                </div>
-              );
-            })}
-          </div>
-          <button type="button" className="btn btn-dark btn-lg">
-            <a href="seleccionar">Atrás</a>
-          </button>
-        </section>
-      
+      <section className="contenedorEjercicios  ">
+        <h1>ENTRENATE JMS</h1>
+        <div className="contenedorEjercicios__lista">
+          {data.infogimnasio.map((elemento, indice) => {
+            return (
+              <div className="carta">
+                <li
+                  key={indice}
+                  className="contenedorEjercicios__lista--elemento"
+                >
+                  {elemento.nomejercicio}
+                </li>
+                <img src={elemento.imagen} alt="Img"></img>
+                {elemento.ejercicios.map((elemento, indice) => {
+                  return (
+                    <div>
+                      <li key={indice}>{elemento.descripcion}</li>
+                    </div>
+                  );
+                })}
+                <button type="button" className="btn btn-dark btn-lg btn-block">
+                  <a href={elemento.ruta}>¿Cómo se hace?</a>{" "}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <button type="button" className="btn btn-dark btn-lg">
+          <a href="seleccionar">Atrás</a>
+        </button>
+      </section>
     );
   }
 }
